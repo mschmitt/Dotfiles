a1cf8a8321eec65b93f0085ea3eab5078bfb9517
Martin Schmitt <mas@scsy.de>
2017-11-08 19:50:13 +0100

    Dir renamed, deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,28 @@ function cfgsync {
 	if [[ $? -eq 0 ]]
 	then
 		echo "$GITCFG: Nothing to do."
-		exit 0
-	fi
-	read -p "Accept Y/N? " -r
-	if [[ $REPLY =~ ^[Yy] ]]
-	then
-		install -b "$GITCFG" "$LOCCFG"
-		echo "$GITCFG: Updated."
+	else
+		read -p "Accept Y/N? " -r
+		if [[ $REPLY =~ ^[Yy] ]]
+		then
+			install -b "$GITCFG" "$LOCCFG"
+			echo "$GITCFG: Updated."
+		fi
 	fi
 }
 
 DIFF=colordiff
 which $DIFF >/dev/null || DIFF=diff
-
+cd $(dirname $0)
 case $(uname -s) in
 	"Darwin"|"Linux"|"FreeBSD")
+		echo "Platform is Darwin, Linux or FreeBSD"
 		cfgsync Vim/vimrc "$HOME/.vimrc"
+		;;
+	"CYGWIN_NT-10.0")
+		echo "Platform is Cygwin"
+		cfgsync Vim/vimrc "$HOME/.vimrc"
+		cfgsync Vim/vimrc "$USERPROFILE/_vimrc"
+		cfgsync Terminals/minttyrc "$HOME/.minttyrc"
 		;;
 esac
