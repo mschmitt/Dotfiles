d4c8b2a17218aaa9c810395ba2589834d19ea7a7
Martin Schmitt <mas@scsy.de>
2017-11-11 01:07:33 +0100

    Cleanup

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ function cfgsync {
 	$DIFF --new-file "$GITCFG" "$LOCCFG" 
 	if [[ $? -eq 0 ]]
 	then
-		echo "$GITCFG: Nothing to do."
+		echo "$GITCFG -> $LOCCFG: Nothing to do."
 	else
 		read -p "Accept $LOCCFG Y/N? " -r
 		if [[ $REPLY =~ ^[Yy] ]]
 		then
 			install -b "$GITCFG" "$LOCCFG"
-			echo "$GITCFG: Updated."
+			echo "$GITCFG -> $LOCCFG: Updated."
 		fi
 	fi
 }
@@ -20,22 +20,23 @@ function cfgsync {
 DIFF=colordiff
 which $DIFF >/dev/null || DIFF=diff
 cd $(dirname $0)
+
+cfgsync Vim/vimrc "$HOME/.vimrc"
+test -d "$HOME/.bashrc.d/" || mkdir "$HOME/.bashrc.d/"
+for FILE in $(ls Bash/bashrc.d/*bash); do
+	cfgsync "$FILE" "$HOME/.bashrc.d/$(basename $FILE)"
+done
+
 case $(uname -s) in
 	"Darwin"|"Linux"|"FreeBSD")
 		echo "Platform is Darwin, Linux or FreeBSD"
-		cfgsync Vim/vimrc "$HOME/.vimrc"
 		;;
 	"CYGWIN_NT-10.0")
 		echo "Platform is Cygwin"
-		cfgsync Vim/vimrc "$HOME/.vimrc"
 		cfgsync Vim/vimrc "$USERPROFILE/_vimrc"
 		cfgsync Terminals/minttyrc "$HOME/.minttyrc"
 		cfgsync Cygwin/bashrc "$HOME/.bashrc"
 		test -d "$HOME/bin" || mkdir "$HOME/bin"
 		cfgsync Cygwin/ssh-agent-bootstrap "$HOME/bin/ssh-agent-bootstrap"
-		mkdir -p "$HOME/.bashrc.d/"
-		for FILE in $(ls Bash/bashrc.d/*bash); do
-			cfgsync "$FILE" "$HOME/.bashrc.d/$(basename $FILE)"
-		done
 		;;
 esac
