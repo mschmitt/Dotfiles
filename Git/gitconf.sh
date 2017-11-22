b047a05966564433f93291d0bdf192896386fe71
Martin Schmitt <mas@scsy.de>
2017-11-22 19:20:22 +0100

    Configure bcompare as git difftool

--- Git/gitconf.sh
+++ Git/gitconf.sh
@@ -2,3 +2,13 @@
 type git 2>/dev/null|| exit
 git config --global push.default simple
 git config --global help.autocorrect 20
+
+if type bcompare 2>/dev/null
+then
+	git config --global diff.tool bc3
+	git config --global difftool.bc3.trustExitCode true
+	git config --global difftool.prompt false
+	git config --global merge.tool bc3
+	git config --global mergetool.bc3.trustExitCode true
+	git config --global mergetool.prompt false
+fi
