ef5788f4d6f14cda93f3038293e3c12ad89a56fb
Martin Schmitt <mas@scsy.de>
2018-03-08 14:58:54 +0100

    Updated poke function

--- Bash/bashrc.d/0600_functions.bash
+++ Bash/bashrc.d/0600_functions.bash
@@ -1,5 +1,19 @@
-# poke() - Send generic message to my mobile, e.g. after a given task has finished.
+# poke() - Send generic message to my mobile, e.g. after a long-running task has finished.
+#
+# Inspired by: https://twitter.com/shezoidic/status/966663437001592832
+#
+# /path/to/long-running; poke
+#         - OR -
+# poke /path/to/long-running
+
 function poke {
+	MESSAGE="$(tty) • $(hostname -s)"
+	if [[ "$*" ]]
+	then
+		"$@"
+		RC=$?
+		MESSAGE="$1 • exit $RC • $(tty) • $(hostname -s)"
+	fi
 	if [[ -s ~/.poke.pushover ]]
 	then
 		source ~/.poke.pushover
@@ -7,7 +21,7 @@ function poke {
 		curl -s --form-string token="$PUSHOVER_POKE_APP" \
 			--form-string title="POKE" \
 			--form-string user="$PUSHOVER_POKE_USER" \
-			--form-string message="from $(tty) on $(hostname -s)" \
+			--form-string message="$MESSAGE" \
 			https://api.pushover.net/1/messages.json > "$TEMPFILE"
 		PUSHOVER_POKE_OUT=$(cat "$TEMPFILE")
 		echo "Call to Pushover returned: $? ($PUSHOVER_POKE_OUT)"
