3bb2bcf746acfaffe8cacbe219555546ca49fe66
Martin Schmitt <mas@scsy.de>
2018-05-31 14:30:57 +0200

    Foolish experiments with xterm and GNU screen escapes

--- Bash/bashrc.d/0200_environment.bash
+++ Bash/bashrc.d/0200_environment.bash
@@ -4,5 +4,6 @@ export SVN_EDITOR=vim
 export TMOUT=3600
 export NMON=cnd
 export LANG=en_US.utf-8
+export PROMPT_COMMAND='echo -ne "\ek$HOSTNAME\e\\"; echo -ne "\e]0;$HOSTNAME\a"'
 
 # vim: filetype=sh
