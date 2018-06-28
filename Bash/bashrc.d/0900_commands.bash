a8a712e7b5923b52547475d1c97a09201b7b9e7e
Martin Schmitt <mas@scsy.de>
2018-06-28 21:55:31 +0200

    Create /cygdrive symlinks in /

--- Bash/bashrc.d/0900_commands.bash
+++ Bash/bashrc.d/0900_commands.bash
@@ -9,4 +9,14 @@ else
 	ssh-add -l >/dev/null 2>&1 || ssh-add
 fi
 
+# Symlinks for the /cygdrive hierarchy
+if [[ "$UNAME_S" == "CYGWIN_NT-10.0" ]]
+then
+	for DRIVE in $(ls /cygdrive)
+	do 
+		[[ -L "/$DRIVE" ]] || 
+		  ln --verbose --symbolic --no-dereference "/cygdrive/$DRIVE" "/$DRIVE"
+	done
+fi
+
 # vim: filetype=sh
