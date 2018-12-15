d00d3a109b33d1a28bfb3e967cbc3b85c3a894b3
Martin Schmitt <mas@scsy.de>
2018-12-15 19:41:56 +0100

    Increase updatecheck frequency, to observe behaviour

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1544823549
+LOCAL_TIMESTAMP=1544899316
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
@@ -10,7 +10,7 @@ SCRIPTTIME=$(stat $SCRIPTFILE --format '%Y')
 NOWTIME=$(date +%s)
 
 let AGE=$NOWTIME-$SCRIPTTIME
-let MINAGE=3600*24*7
+let MINAGE=60*60
 
 if [[ $AGE -gt $MINAGE ]]
 then
