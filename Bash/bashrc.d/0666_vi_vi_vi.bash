a57225be04d15e23e5b0946f0e53627243f03c11
Martin Schmitt <mas@scsy.de>
2019-07-31 14:23:31 +0200

    Forgot some alternative text editors

--- Bash/bashrc.d/0666_vi_vi_vi.bash
+++ Bash/bashrc.d/0666_vi_vi_vi.bash
@@ -4,8 +4,10 @@ then
         return
 fi
 
-alias emacs=vi
 alias joe='echo "core dumped." #'
 alias nano='echo "core dumped." #'
+alias pico='echo "core dumped." #'
+alias mcedit='echo "core dumped." #'
+alias emacs='ed'
 
 # vim: filetype=sh
