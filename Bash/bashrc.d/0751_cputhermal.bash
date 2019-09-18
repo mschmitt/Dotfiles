85b2cb5d44fad16e2194ff8184ecb19ecd7ff4b7
Martin Schmitt <mas@scsy.de>
2019-09-18 15:32:35 +0200

    Actually add missing file

---
+++ Bash/bashrc.d/0751_cputhermal.bash
@@ -0,0 +1,33 @@
+# Skip if running non-interactively
+if [[ ! -t 0 ]]
+then
+        return
+fi
+
+if [[ $UID -ne 0 ]]
+then
+	return
+fi
+
+if [[ "$UNAME_S" != "Linux" ]]
+then
+	return
+fi
+
+function temperature {
+	local DIR
+	local TYPE
+	local RAWTEMP
+	for DIR in /sys/class/thermal/thermal_zone*
+	do
+		read -r TYPE < "$DIR/type"
+		if [[ "$TYPE" =~ (x86_pkg_temp|bcm2835_thermal) ]]
+		then
+			read -r RAWTEMP < "$DIR/temp"
+			printf "CPU core temperature: %s°\n" "$(( $RAWTEMP / 1000 ))"
+		fi
+	done
+}
+temperature
+
+# vim: filetype=sh
