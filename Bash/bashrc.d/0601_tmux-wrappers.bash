9519bdefbebe50ce8569179feb527b17aa0cfd90
Martin Schmitt <mas@scsy.de>
2019-10-17 23:15:49 +0200

    Sample tmux clusterssh wrapper

---
+++ Bash/bashrc.d/0601_tmux-wrappers.bash
@@ -0,0 +1,18 @@
+# Skip if running non-interactively
+if [[ ! -t 0 ]]
+then
+        return
+fi
+
+function vmailmux() {
+	tmux new-session -d 'ssh vmx1 -l root -A'
+	tmux split-window 'ssh vmx2 -l root -A'
+	tmux split-window 'ssh vmail -l root -A'
+	tmux set-option -g synchronize-panes on
+	tmux set-option -g allow-rename off 
+	tmux select-layout even-vertical
+	tmux rename-window 'vmx1/vmx2/vmail'
+	tmux attach-session
+}
+
+# vim: filetype=sh
