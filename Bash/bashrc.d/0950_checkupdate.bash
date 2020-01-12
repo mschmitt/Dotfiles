3beff774a254fae00bde890393a889008b08318e
Martin Schmitt <mas@scsy.de>
2020-01-12 12:09:38 +0100

    mvtop function

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1578686422
+LOCAL_TIMESTAMP=1578827378
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
