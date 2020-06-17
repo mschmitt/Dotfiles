e9ea31e106d0789676e90954cd4d5f3f8b268c17
Martin Schmitt <martin@dell-netbook.localnet.name>
2020-06-17 22:33:00 +0200

    Naive Xresources to Lxterminal color config converter

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1592425939
+LOCAL_TIMESTAMP=1592425980
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
