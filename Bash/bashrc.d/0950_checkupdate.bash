6aff0c1598ab64cf8ebde4e084ec668b9765c704
Martin Schmitt <mas@scsy.de>
2020-07-09 23:01:38 +0200

    Clarification

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1593234466
+LOCAL_TIMESTAMP=1594328498
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
