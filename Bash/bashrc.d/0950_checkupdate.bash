b7548ec42230dd286ac1e60fe5d8f519e8bb968e
Martin Schmitt <mas@scsy.de>
2020-08-01 12:36:49 +0200

    alias top='top -c'

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1594328498
+LOCAL_TIMESTAMP=1596278209
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
