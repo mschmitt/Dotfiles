bcb54fbf46173ddd42a0dda3aeb2089e90512bbc
Martin Schmitt <mas@scsy.de>
2020-08-05 20:23:36 +0200

    Silence my own helpful hints. This is brilliant.

--- Bash/bashrc.d/0910_helpful.bash
+++ Bash/bashrc.d/0910_helpful.bash
@@ -5,12 +5,12 @@ then
 fi
 
 # Point out generally useful stuff that is missing on this system.
-if [[ ! -z "$debian" ]]
+if [[ ! -z "$debian" && ! -e ~/.dotfiles-nohelp ]]
 then
 	[[ -x $(command -v locate) ]]   || echo "locate is missing here."
 	[[ -x $(command -v apt-file) ]] || echo "apt-file is missing here."
 	[[ -x $(command -v sar) ]]      || echo "sysstat is missing here."
-	grep -q 'ENABLED="true"' /etc/default/sysstat || echo "sadc is disabled here."
+	grep -q 'ENABLED="true"' /etc/default/sysstat 2>/dev/null || echo "sadc is disabled here."
 fi
 
 # vim: filetype=sh
