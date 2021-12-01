58bade2f7d7ade050cef8fe784f9612c56826b26
Martin Schmitt <mas@scsy.de>
2021-12-01 12:14:50 +0100

    Git verbose commit

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1637872667
+LOCAL_TIMESTAMP=1638357290
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
