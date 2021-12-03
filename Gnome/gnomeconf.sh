51e4131ed9eebce13ad32f6383044b073b86df98
Martin Schmitt 🐧 <mas@scsy.de>
2021-12-03 22:22:18 +0100

    Fix and improve gnome detection

--- Gnome/gnomeconf.sh
+++ Gnome/gnomeconf.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-type git 2>/dev/null|| exit
+pkill -0 dbus-daemon -U "${LOGNAME}" || exit
 gsettings set org.gnome.desktop.wm.preferences audible-bell false
 gsettings set org.gnome.desktop.wm.preferences visual-bell true
 gsettings set org.gnome.desktop.wm.preferences visual-bell-type frame-flash
