00a277825a813223667b52e802e1677a2ffd0af3
Martin Schmitt 👽 <mas@scsy.de>
2021-12-18 23:49:47 +0100

    Inconsolata

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1639865005
+LOCAL_TIMESTAMP=1639867787
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
