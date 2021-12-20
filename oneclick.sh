a0ff21db27d5ab0aac5ace67bd96efa14e832286
Martin Schmitt <mas@scsy.de>
2021-12-20 10:29:44 +0100

    World famous one-click installer

---
+++ oneclick.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -o errexit
+
+function errorexit() {
+	trap - ERR
+	printf "Error on line %s\n" "$(caller)"
+	exit 1
+}
+trap errorexit ERR
+
+sudo apt-get -y install curl git make shellcheck vim-nox
+
+cd $HOME
+
+git clone https://github.com/mschmitt/Dotfiles
+cd Dotfiles
+git remote set-url        origin https://github.com/mschmitt/Dotfiles
+git remote set-url --push origin git@github.com:mschmitt/Dotfiles
+make
+
+cat >> $HOME/.bashrc << 'Here'
+# Setup bash environment only if running as an interactive shell
+[[ "$-" =~ i ]] && for FILE in $(ls ~/.bashrc.d/*bash); do source $FILE; done
+Here
+
+Vim/install-plugins.sh
