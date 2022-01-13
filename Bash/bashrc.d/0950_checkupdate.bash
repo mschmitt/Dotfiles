90674bb42b21c7dfe90faac55097ff17beb288a8
Martin Schmitt 🍏 <mas@scsy.de>
2022-01-13 21:12:48 +0100

    Edit ansible.cfg using crudini

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1642104032
+LOCAL_TIMESTAMP=1642104768
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
