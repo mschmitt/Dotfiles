0c5914ab13b323939b010e5b0bba7cc9cde3bd00
Martin Schmitt 🐧 <mas@scsy.de>
2022-02-08 23:25:04 +0100

    Suppress ansible deprecation warnings

--- Misc/ansible.sh
+++ Misc/ansible.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-### pip3 install crudini
+### pip3 install crudini / apt-get install crudini
 if type crudini >/dev/null 2>&1
 then
 	crudini --set ~/.ansible.cfg defaults interpreter_python auto_silent
+	crudini --set ~/.ansible.cfg defaults deprecation_warnings false
 fi
 
