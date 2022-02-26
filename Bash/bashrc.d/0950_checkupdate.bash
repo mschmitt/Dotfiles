4b784a140f8e8938fb1f859e9c95d71a167d88e7
Martin Schmitt 🐧 <mas@scsy.de>
2022-02-26 10:32:51 +0100

    Alias userctl=systemctl --user and bash completion for aliases

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1644566212
+LOCAL_TIMESTAMP=1645867971
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
