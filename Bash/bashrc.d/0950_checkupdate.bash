b3f2590c1244c04b776768367a10dfde9fd01ed0
Martin Schmitt <martin.schmitt@external.t-systems.com>
2022-03-04 17:42:23 +0100

    Add actual Gnome Keyring Desktop unit

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1646410336
+LOCAL_TIMESTAMP=1646412143
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
