26c4b551851e1d6ffeda048e1a99433e279b206b
Martin Schmitt 👽 <mas@scsy.de>
2022-03-04 20:46:13 +0100

    More Gnome settings

--- Gnome/gnomeconf.sh
+++ Gnome/gnomeconf.sh
@@ -3,3 +3,7 @@ pkill -0 dbus-daemon -U "${LOGNAME}" || exit
 gsettings set org.gnome.desktop.wm.preferences audible-bell false
 gsettings set org.gnome.desktop.wm.preferences visual-bell true
 gsettings set org.gnome.desktop.wm.preferences visual-bell-type frame-flash
+gsettings set org.gnome.desktop.peripherals.touchpad click-method fingers
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
+gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
