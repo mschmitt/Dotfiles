134f93da3d154beb49a490c45017606cfe1817d9
Martin Schmitt 👽 <mas@scsy.de>
2022-03-10 21:16:37 +0100

    Alt+X for terminal like in the good old days

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1646423681
+LOCAL_TIMESTAMP=1646943397
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
--- Gnome/gnomeconf.sh
+++ Gnome/gnomeconf.sh
@@ -8,3 +8,4 @@ gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
 gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us+altgr-intl')]"
+gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Alt>x']"
