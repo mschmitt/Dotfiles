db41a0d731a61be02d2ef7ea5f984054f836e6f6
Martin Schmitt 🐧 <mas@scsy.de>
2022-03-23 08:41:41 +0100

    US/DE keyboard interoperable passwords

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1647892209
+LOCAL_TIMESTAMP=1648021301
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
