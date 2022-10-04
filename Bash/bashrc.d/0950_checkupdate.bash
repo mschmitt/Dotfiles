db15a7679256c9b5f7f4837774c4bb573e50d543
Martin Schmitt 👽 <mas@scsy.de>
2022-10-04 21:22:49 +0200

    Add private dotfiles

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1664702117
+LOCAL_TIMESTAMP=1664911369
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
