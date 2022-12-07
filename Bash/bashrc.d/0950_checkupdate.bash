4a4738a2b470ccc428d66c36cdc9b8d6937f3814
Martin Schmitt ❄️ <mas@scsy.de>
2022-12-07 19:38:57 +0100

    Add wallpaper changer

--- Bash/bashrc.d/0950_checkupdate.bash
+++ Bash/bashrc.d/0950_checkupdate.bash
@@ -1,7 +1,7 @@
 # Check for updates
 
 # This field updated by git pre-commit hook:
-LOCAL_TIMESTAMP=1670437202
+LOCAL_TIMESTAMP=1670438337
 
 # Check for update no more frequently than every 7 days
 # Keep track by touching this file itself.
