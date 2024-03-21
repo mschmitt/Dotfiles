f483ba986bed38f39f2e25c42053f8413e8b2ea0
Martin Schmitt 🌙🦞 <mas@scsy.de>
2024-03-21 22:23:22 +0100

    Github codespace

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ "${LOGNAME}" != 'codespace' ]]
-then
-	echo "This is a bootstrap script for github codespaces."
-	echo "https://docs.github.com/en/codespaces/setting-your-user-preferences/personalizing-github-codespaces-for-your-account#dotfiles"
-	exit 1
-fi
-
 sudo locale-gen de_DE.utf8
 
 sudo apt-get -y update
