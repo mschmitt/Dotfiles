c2699f634b571c81ba6c2393f10690f1311056f6
Martin Schmitt 🌙🦞 <mas@scsy.de>
2024-03-21 21:44:17 +0100

    locale-gen for codespace

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-env > /var/tmp/1
+if [[ ! -v GITHUB_CODESPACE_TOKEN ]]
+then
+	echo "This is a bootstrap script for github codespaces."
+	echo "https://docs.github.com/en/codespaces/setting-your-user-preferences/personalizing-github-codespaces-for-your-account#dotfiles"
+	exit 1
+else
+	sudo locale-gen de_DE.utf8
+fi
