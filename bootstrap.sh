63b6ccb31c02e42ccfe4dba11f899b1ad32e85ff
Martin Schmitt 🌙🦞 <mas@scsy.de>
2024-03-21 21:52:33 +0100

    Codespace script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ ! -v GITHUB_CODESPACE_TOKEN ]]
+if [[ "${LOGNAME}" != 'codespace' ]]
 then
 	echo "This is a bootstrap script for github codespaces."
 	echo "https://docs.github.com/en/codespaces/setting-your-user-preferences/personalizing-github-codespaces-for-your-account#dotfiles"
 	exit 1
-else
-	sudo locale-gen de_DE.utf8
 fi
+
+sudo locale-gen de_DE.utf8
+
+curl -s https://raw.githubusercontent.com/mschmitt/Dotfiles/master/Misc/yolo.sh | bash
+
