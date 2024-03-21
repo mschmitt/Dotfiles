7eea1d9d918132b6572c1158eee85fa6580711b4
Martin Schmitt 🌙🦞 <mas@scsy.de>
2024-03-21 22:20:40 +0100

    Github bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,6 @@ sudo locale-gen de_DE.utf8
 sudo apt-get -y update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-curl -s https://raw.githubusercontent.com/mschmitt/Dotfiles/master/Misc/yolo.sh | bash
+curl -s https://raw.githubusercontent.com/mschmitt/Dotfiles/master/Misc/yolo.sh | bash || true
 
 touch  ~/.dotfiles-nohelp
